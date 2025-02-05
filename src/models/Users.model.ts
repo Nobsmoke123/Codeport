ff2a36b537bc800d32cf94ab9079ce126eb65b5f
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-05 23:49:16 +0100

    Add User model with type definitions and enums for SocialProvider and UserRole

---
+++ src/models/Users.model.ts
@@ -0,0 +1,23 @@
+enum SocialProvider {
+  Google = 'google',
+  Twitter = 'twitter',
+  Facebook = 'facebook',
+  None = 'none',
+}
+
+enum UserRole {
+  Admin = 'admin',
+  User = 'user',
+}
+
+export type User = {
+  id: string;
+  fullname: string;
+  username: string;
+  email: string;
+  password?: string;
+  socialProvider: SocialProvider;
+  role: UserRole;
+  createdAt: Date;
+  updatedAt: Date;
+};
