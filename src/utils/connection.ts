4ea62214ce36dbe9f3fa3bbf22ec6422bfea6195
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-07 23:53:22 +0100

    Update .env.sample and enhance MongoDB connection utility with error handling

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -2,10 +2,16 @@ import mongoose from 'mongoose';
 
 export const Connection = async () => {
   try {
-    const MONGODB_URL = process.env.MONGODB_URL;
+    const MONGODB_URL = process.env.MONGODB_URL || '';
 
-    await mongoose.connect('');
+    if (MONGODB_URL === '') {
+      throw new Error('MONGODB_URL is required for the application to work!');
+    }
+
+    const connection = await mongoose.connect(MONGODB_URL);
+    console.log('Database connected successfully.');
   } catch (error) {
     console.log(error);
+    throw error; // Handle it in the index.ts file
   }
 };
