a5ddd9e9c3f086723900ae0967c5f48165c10120
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-07 23:12:00 +0100

    Add morgan for logging and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Application } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
 import authRoute from './routes/auth';
 
 const app: Application = express();
 
 app.use(helmet());
+app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json());
 
 app.use('/auth', authRoute);
 
