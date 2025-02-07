3cba90105f3ab3ce4ed43035ce75c73ab79d25c4
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-08 00:39:23 +0100

    Refactor DatabaseConnection class to improve connection handling and add closeConnection method

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,29 +1,50 @@
 import mongoose from 'mongoose';
 
 class DatabaseConnection {
-  CONNECTION_URL: string;
+  private client?: mongoose.Connection;
 
-  CONNECTION = '';
-
-  constructor() {
-    this.CONNECTION_URL = process.env.MONGODB_URL || '';
-  }
-
-  async Connect() {
+  async connect() {
     try {
-      if (this.CONNECTION_URL === '') {
+      const MONGO_URL = process.env.MONGO_URL || '';
+
+      if (MONGO_URL === '') {
         throw new Error('MONGODB_URL is required for the application to work!');
       }
 
-      this.CONNECTION = await mongoose.connect(this.CONNECTION_URL);
-      console.log('Database connected successfully.');
+      // Check if there's already a database connection
+      if (mongoose.connection.readyState === 1) {
+        console.log('Database is already connected.');
+        return;
+      }
+
+      const db = await mongoose.connect(MONGO_URL);
+
+      if (db.connection.readyState === 1) {
+        console.log('Database connected successfully.');
+        this.client = db.connection;
+      }
     } catch (error) {
-      console.log(error);
+      console.log('Database connection error: ', error);
       throw error; // Handle it in the index.ts file
     }
   }
 
-  async ConnectionClose() {}
+  async closeConnection() {
+    try {
+      // Check if there's a database connection to close.
+      if (!this.client) {
+        console.log('No active database connection to close.');
+        return;
+      }
+
+      // Close the database connection
+      await this.client?.close();
+      console.log('Database connection closed.');
+    } catch (error) {
+      console.log('Database connection error: ', error);
+      throw error;
+    }
+  }
 }
 
-export const Connection = async () => {};
+export default new DatabaseConnection();
